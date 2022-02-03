0f07a7ce217649de00d6d4006604216b8554444a
CaedenM <CaedenM@github.com>
2022-02-03 15:33:19 -0500

    Improved JSON schema

--- validateJSON.py
+++ validateJSON.py
@@ -10,52 +10,83 @@ SECTIONS = os.path.join(HERE, 'sections')
 SCHEMA_NAME = 'xrna-schema.json'
 LOGGER = logging.getLogger(__name__)
 
-# 1. Validates that there are no repeated nucleotide indices per RNA molecule.
+# 1. Validates that there are no repeated nucleotide (residue) indices per RNA molecule.
 # 2. Validates that there are no gaps betwwen nucleotide indices per RNA molecule.
-# 3. Validates that no basePairs refer to an invalid nucleotide index (Nucleotide indices outside the sequence resId range).
-# 4. Validates that no labels or annotations refer to an invalid nucleotide index (nucleotide indices outside the sequence resId range).
+# 3. Validates that no basePairs refer to an invalid nucleotide index (Nucleotide indices outside the sequence residueIndex range).
+# 4. Validates that no labels or annotations refer to an invalid nucleotide index (nucleotide indices outside the sequence residueIndex range).
 def validate_indices(data):
-    rna_complexes = data.get("rna complexes", [])
+    rna_complexes = data.get("rnaComplexes", [])
     for rna_complex in rna_complexes:
-        rna_molecules = rna_complex.get("rna molecules", [])
+        rna_molecules = rna_complex.get("rnaMolecules", [])
         for rna_molecule in rna_molecules:
             sequence = rna_molecule.get("sequence", [])
-            seen_residue_ids = set()
+            seen_residue_indices = set()
             for nucleotide in sequence:
-                if "resId" in nucleotide:
-                    res_id = nucleotide["resId"]
-                    if res_id in seen_residue_ids:
-                        yield js.ValidationError("Within a single RNA molecule, resId values must not be repeated.")
+                if "residueIndex" in nucleotide:
+                    residue_index = nucleotide["residueIndex"]
+                    if residue_index in seen_residue_indices:
+                        yield js.ValidationError("Within a single RNA molecule, residueIndex values must not be repeated.")
                     else:
-                        seen_residue_ids.add(res_id)
+                        seen_residue_indices.add(residue_index)
             basePairs = rna_molecule.get("basePairs", [])
             for basePair in basePairs:
-                if "resId1" in basePair and "resId2" in basePair:
-                    res_id_1 = basePair["resId1"]
-                    res_id_2 = basePair["resId2"]
-                    absolute_distance = abs(res_id_1 - res_id_2)
+                if "residueIndex1" in basePair and "residueIndex2" in basePair:
+                    residue_index_1 = basePair["residueIndex1"]
+                    residue_index_2 = basePair["residueIndex2"]
+                    absolute_distance = abs(residue_index_1 - residue_index_2)
                     if absolute_distance == 0:
                         yield js.ValidationError("A nucleotide may not base pair with itself.")
                     if absolute_distance == 1:
                         yield js.ValidationError("A nucleotide may not base pair with adjacent nucleotides.")
-                    if not res_id_1 in seen_residue_ids:
-                        yield js.ValidationError("resId1 values within basePair objects must reference a valid nucleotide resId.")
-                    if not res_id_2 in seen_residue_ids:
-                        yield js.ValidationError("resId2 values within basePair objects must reference a valid nucleotide resId.")
-            minimum_residue_id = min(seen_residue_ids, key=lambda x: int(x))
-            maximum_residue_id = max(seen_residue_ids, key=lambda x: int(x))
-            labels_and_annotations = rna_molecule.get("labelsAndAnnotations", [])
-            if len(sequence) > 0 and (maximum_residue_id - minimum_residue_id) != len(sequence) - 1:
-                yield js.ValidationError("RNA molecule sequences must not leave gaps between residue ids")
-            for label_or_annotation in labels_and_annotations:
-                if "resId" in label_or_annotation:
-                    res_id = label_or_annotation["resId"]
-                    if not res_id in seen_residue_ids:
-                        yield js.ValidationError("ResId values within labels and annotations must reference a valid nucleotide resId.")
-            
+                    if not residue_index_1 in seen_residue_indices:
+                        yield js.ValidationError("residueIndex1 values within basePair objects must reference a valid nucleotide residueIndex.")
+                    if not residue_index_2 in seen_residue_indices:
+                        yield js.ValidationError("residueIndex2 values within basePair objects must reference a valid nucleotide residueIndex.")
+            if len(sequence) > 0:
+                minimum_residue_id = min(seen_residue_indices, key=lambda residue_index: int(residue_index))
+                maximum_residue_id = max(seen_residue_indices, key=lambda residue_index: int(residue_index))
+                if maximum_residue_id - minimum_residue_id != len(sequence) - 1:
+                    yield js.ValidationError("RNA molecule sequences must not leave gaps between residue indices")
+            labels = rna_molecule.get("labels", [])
+            for label in labels:
+                if "residueIndex" in label:
+                    residue_index = label["residueIndex"]
+                    if not residue_index in seen_residue_indices:
+                        yield js.ValidationError("residueIndex values within labels must reference a valid nucleotide residueIndex.")
+
+# Validates that all referenced classes are present within the top-level "classes" object
+def validate_classes(data):
+    seen_classes = set()
+    rna_complexes = data.get("rnaComplexes", [])
+    for rna_complex in rna_complexes:
+        rna_molecules = rna_complex.get("rnaMolecules", [])
+        for rna_molecule in rna_molecules:
+            sequence = rna_molecule.get("sequence", [])
+            for residue in sequence:
+                classes = residue.get("classes", [])
+                for class_ in classes:
+                    seen_classes.add(class_)
+            labels = rna_molecule.get("labels", [])
+            for label in labels:
+                if "labelLine" in label:
+                    label_line = label["labelLine"]
+                    classes = label_line.get("classes", [])
+                    for class_ in classes:
+                        seen_classes.add(class_)
+                if "labelContent" in label:
+                    label_content = label["labelContent"]
+                    classes = label_content.get("classes", [])
+                    for class_ in classes:
+                        seen_classes.add(class_)
+    classes = set()
+    for class_ in data.get("classes", []):
+        if "name" in class_:
+            classes.add(class_["name"])
+    for seen_class in seen_classes:
+        if not seen_class in classes:
+            yield js.ValidationError("All referenced classes must be defined within the top-level \"classes\" array.")
 
 def validate(data, schema_path, sections_path):
-
     with open(schema_path, 'r') as raw:
         schema = json.load(raw)
 
@@ -72,8 +103,13 @@ def validate(data, schema_path, sections_path):
         counts[error.validator] += 1
         found = True
         LOGGER.error(error.message)
-
+    
     for error in validate_indices(data):
+        counts[error.validator] += 1
+        found = True
+        LOGGER.error(error.message)
+
+    for error in validate_classes(data):
         counts[error.validator] += 1
         found = True
         LOGGER.error(error.message)
